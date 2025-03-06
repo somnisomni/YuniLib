68cd5b83dd268719b97b8fb76bb67c8dfcf9cd64
somni <me@somni.one>
2025-03-07 01:03:05 +0900

    Add gizmo shape drawer utility class
    Signed-off-by: somni <me@somni.one>

---
+++ Runtime/Gizmo/GizmoShapeDrawer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Somni.YuniLib.Gizmo {
+    public static class GizmoShapeDrawer {
+        public static void DrawCircularSector(Vector3 center,
+            Vector3 normal,
+            Vector3 direction,
+            float angleDegree,
+            float radius,
+            Color color,
+            float colorMaxAlpha = 0.5f) {
+#if UNITY_EDITOR
+            Color originalHandleColor = Handles.color;
+
+            Handles.color = new Color(color.r, color.g, color.b, Mathf.Min(color.a, colorMaxAlpha));
+            Handles.DrawSolidArc(center, normal, direction, angleDegree / 2, radius);
+            Handles.DrawSolidArc(center, normal, direction, -angleDegree / 2, radius);
+            Handles.color = originalHandleColor;
+#endif
+        }
+
+        public static void DrawCircularSector(Transform target,
+            Vector3 normal,
+            float angleDegree,
+            float radius,
+            Color color,
+            float colorMaxAlpha = 0.5f) {
+            DrawCircularSector(target.position, normal, target.forward, angleDegree, radius, color, colorMaxAlpha);
+        }
+    }
+}
