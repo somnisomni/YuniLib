1730fb03546669c9bcf02b81e674b21945f66e15
somni <me@somni.one>
2025-04-13 00:47:13 +0900

    Add LayerMask extensions

---
+++ Runtime/Extensions/LayerMaskExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace Somni.YuniLib.Extensions {
+    public static class LayerMaskExtensions {
+        public static bool Contains(this LayerMask layerMask, int targetLayer) {
+            return (layerMask & (1 << targetLayer)) != 0;
+        }
+        
+        public static bool ContainsAll(this LayerMask layerMask, LayerMask targetLayerMask) {
+            return (layerMask & targetLayerMask) == targetLayerMask;
+        }
+        
+        public static bool ContainsAll(this LayerMask layerMask, params int[] targetLayers) {
+            return targetLayers.All(targetLayer => layerMask.Contains(targetLayer));
+        }
+    }
+}
