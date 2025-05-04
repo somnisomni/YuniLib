37cdd8fa7e2398abb3bfb00e021acac41badced0
somni <me@somni.one>
2025-05-04 21:54:46 +0900

    Call `LookAt` only if the target position/rotation has changed

--- Runtime/UI/Billboard.cs
+++ Runtime/UI/Billboard.cs
@@ -1,26 +1,36 @@
-﻿using UnityEngine;
+﻿using Somni.YuniLib.Inspector;
+using UnityEngine;
 
 namespace Somni.YuniLib.UI {
     [AddComponentMenu("somni YuniLib/UI/Billboard")]
     public class Billboard : MonoBehaviour {
+        [HeaderEx("Target", HeaderExStyle.H1)]
         [SerializeField]
         [Tooltip("A transform that will be rotated at this position. This usually be the transform of Camera. If this is not specified, the main camera will be used.")]
-        private Transform target;
+        public Transform lookTarget;
+
+        private Vector3 lastPositionOfTarget;
+        private Quaternion lastRotationOfTarget;
 
         private void Start() {
-            if(!target) {
-                target = Camera.main?.transform;
+            if(!lookTarget) {
+                lookTarget = Camera.main?.transform;
             }
         }
 
         private void LateUpdate() {
-            if(!target) {
+            if(!lookTarget) {
                 return;
             }
+
+            if(lastPositionOfTarget == lookTarget.position && lastRotationOfTarget == lookTarget.rotation) return;
             
             transform.LookAt(
-                worldPosition: transform.position + (target.rotation * Vector3.forward),
-                worldUp: target.rotation * Vector3.up);
+                worldPosition: transform.position + (lookTarget.rotation * Vector3.forward),
+                worldUp: lookTarget.rotation * Vector3.up);
+            
+            lastPositionOfTarget = lookTarget.position;
+            lastRotationOfTarget = lookTarget.rotation;
         }
     }
 }
